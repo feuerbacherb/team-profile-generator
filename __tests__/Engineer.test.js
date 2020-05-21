33ee5630efafb76fef41bcbadfa238a299c830bc
Brent F <feuerbacherb@gmail.com>
2020-05-21 14:05:12 -0500

    changed the name of the manager to something more appropriate

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,8 @@
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+
+// test to see if the Engineer object can be created
+test('Can the Engineer object be created?', () => {
+   const eng = new Engineer();
+});
+
